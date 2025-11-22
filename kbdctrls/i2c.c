e35e55056f930714c0007af81eab40978f0409b8
Andre Fachat <afachat@gmx.de>
2025-11-22 23:27:20 +0100

    fix hand in i2c code

--- kbdctrls/i2c.c
+++ kbdctrls/i2c.c
@@ -1,5 +1,7 @@
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
 /* commands have:
  * 0: command byte
@@ -16,7 +18,19 @@
 #define	MT_DATA_ACK		0x28
 
 static char txbuf[LP_CMDLEN];
-static int txp;
+static int txlen;
+
+static inline int i2c_wait() {
+	int c = 0;
+	while (!(TWCR & (1<<TWINT)))
+	{
+		c++;
+		if (c > 1000000) {
+			return -1;
+		}
+	}
+	return 0;
+}
 
 void i2c_setup(int addr) {
 
@@ -25,43 +39,48 @@ void i2c_setup(int addr) {
 
 	TWAR	= (addr & 0x7f) << 1;
 
+	// to get to 100kHz, the factor must be > 160
+	// bit 0/1 are bitrate prescaler bits (1/4/16/64)
+	// SCL freq is (CPU clock)/(16+2*TWBR*4^TWPS)
+	TWSR	= 3;	// prescaler is 3 -> factor 64
+	TWBR 	= 2;	// 
+
 	TWCR 	= (1<<TWEN) | (1<<TWINT); 
+
+	TWCR 	= (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 }
-
-int i2c_handle_tx() {
-	unsigned char byt = txbuf[txp];
-	txp++;
-
-	TWDR 	= byt;
-	TWCR	= (1<<TWINT) | (1<<TWEN);
-
-	while (!(TWCR & (1<<TWINT)));
-
-	return txp < 2;
-}
-
 
 
 static int i2c_send_int() {
 
 	TWCR 	= (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	
-	while (!(TWCR & (1<<TWINT)));
+	if (i2c_wait()) {
+		return -1;
+	}
 
 	if ((TWSR & 0xf8) != START) {
 		return -1;
 	}
-	
+
 	TWDR 	= 0x10 << 1;	// write to 0x10
 	TWCR	= (1<<TWINT) | (1<<TWEN);
 
-	while (!(TWCR & (1<<TWINT)));
+	if (i2c_wait()) {
+		return -1;
+	}
 
 	if ((TWSR & 0xf8) != MT_SLA_ACK) {
 		return -1;
 	}
 
-	while (i2c_handle_tx()) {
+	for (int txp = 0; txp < txlen; txp++) {
+		TWDR = txbuf[txp];
+		TWCR = (1<<TWINT | 1<<TWEN);
+
+		if (i2c_wait()) {
+			return -1;
+		}
 		if ((TWSR & 0xf8) != MT_DATA_ACK) {
 			return -1;
 		}
@@ -74,9 +93,15 @@ int i2c_send(int idx) {
 
 	txbuf[0] = 64;	//SP_KEYPRESS
 	txbuf[1] = idx;
-	txp = 0;
+	txlen = 2;
+
+	_delay_ms(1);
 
 	i2c_send_int();
 
+
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+
+	//i2c_wait();
 }
+
