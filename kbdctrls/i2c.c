8a5b00d45ba1350981658a63607500cf6629c5ad
Andre Fachat <afachat@gmx.de>
2026-01-06 00:20:36 +0100

    improved error handling, hopefully fixing the hangs

--- kbdctrls/i2c.c
+++ kbdctrls/i2c.c
@@ -18,16 +18,15 @@
 #define	MT_DATA_ACK		0x28
 
 static char txbuf[LP_CMDLEN];
-static int txlen;
 
 static inline int i2c_wait() {
-	int c = 0;
+	uint32_t c = 0;
 	while (!(TWCR & (1<<TWINT)))
 	{
-		c++;
-		if (c > 1000000) {
-			return -1;
-		}
+//		c++;
+//		if (c > 100000) {
+//			return -1;
+//		}
 	}
 	return 0;
 }
@@ -40,20 +39,33 @@ void i2c_setup(int addr) {
 	TWAR	= (addr & 0x7f) << 1;
 
 	// to get to 100kHz, the factor must be > 160
+	// to get to 20kHz, the factor must be > 800
 	// bit 0/1 are bitrate prescaler bits (1/4/16/64)
 	// SCL freq is (CPU clock)/(16+2*TWBR*4^TWPS)
 	TWSR	= 3;	// prescaler is 3 -> factor 64
-	TWBR 	= 2;	// 
+	TWBR 	= 2;	// 16+2*2*64 = 16+256
+	//TWBR 	= 5;	// 16+2*5*64 = 16+256 = 640
+
+	TWCR 	= (1<<TWEN) | (1<<TWINT); 
+}
+
+static void i2c_reset() {
+
+	TWCR 	= (1<<TWINT);
+
+	_delay_ms(1);
 
 	TWCR 	= (1<<TWEN) | (1<<TWINT); 
 
-	TWCR 	= (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 }
 
+static void i2c_stop() {
 
-static int i2c_send_int() {
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+}
 
-cli();
+static int i2c_send_int(char *buf, int len) {
+
 	TWCR 	= (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	
 	if (i2c_wait()) {
@@ -75,8 +87,8 @@ cli();
 		return -1;
 	}
 
-	for (int txp = 0; txp < txlen; txp++) {
-		TWDR = txbuf[txp];
+	for (int txp = 0; txp < len; txp++) {
+		TWDR = buf[txp];
 		TWCR = (1<<TWINT | 1<<TWEN);
 
 		if (i2c_wait()) {
@@ -86,7 +98,7 @@ cli();
 			return -1;
 		}
 	}
-sei();
+
 	return 0;
 }
 
@@ -94,16 +106,14 @@ int i2c_send(int idx) {
 
 	txbuf[0] = 64;	//SP_KEYPRESS
 	txbuf[1] = idx;
-	txlen = 2;
 
 	_delay_ms(1);
 
-	i2c_send_int();
-
-
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-
-	//i2c_wait();
+	if (i2c_send_int(txbuf, 2)) {
+		i2c_reset();
+	} else {
+		i2c_stop();
+	}
 }
 
 int i2c_send_slock(int idx, int slock) {
@@ -114,15 +124,11 @@ int i2c_send_slock(int idx, int slock) {
 	txbuf[3] = 0;	
 	txbuf[4] = 0;	
 	txbuf[5] = 0;	
-	txlen = 6;
 
-	_delay_ms(1);
-
-	i2c_send_int();
-
-
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-
-	//i2c_wait();
+	if (i2c_send_int(txbuf, 6)) {
+		i2c_reset();
+	} else {
+		i2c_stop();
+	}
 }
 
